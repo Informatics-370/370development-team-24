4fb2009de41876cfe78c3b7b3ee491419263e945
21492362 <u21492362@up.ac.za>
2023-05-15 22:38:06 +0200

    Africanacity Frontend
    Default

---
+++ Africanacity_Frontend/src/app/app-routing.module.ts
@@ -0,0 +1,44 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LoginComponent } from './login/login.component';
+import { OtpComponent } from './login/otp/otp.component';
+import { RegisterComponent } from './login/register/register.component';
+
+// routing for pages
+
+import { HomeComponent } from './administration/home/home.component'; // for home page
+import { MenuComponent } from './administration/menu/menu.component'; // for menu page
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent }, // for home page
+  { path: 'menu', component: MenuComponent }, // for menu page
+  {
+    path: '',
+    component: NavbarComponent
+  },
+  {
+    path: 'navbar',
+    component: NavbarComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'otp',
+   component:OtpComponent
+  },
+   {
+    path: 'register', 
+   component:RegisterComponent
+  },
+  {path: '', 
+  redirectTo: 'login', pathMatch:'full'}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ Africanacity_Frontend/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Africanacity_System';
+
+  toggleLogin = JSON.parse(localStorage.getItem('User')!)
+}
