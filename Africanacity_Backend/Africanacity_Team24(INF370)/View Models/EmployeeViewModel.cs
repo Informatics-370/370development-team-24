a66e74fb4817ea6ffc5c1dc30b7cc3750e583bce
u21481742 <u21481742@tuks.co.za>
2023-05-16 03:31:31 +0200

    Changes made, in the appsettings.json file(Database Connection). Cretaed a AddHelp View Model, ViewHelpVM, AddEmployee ViewModel, ViewEmployeeVm. Created a HelpController. Inputted attributes in the EmployeeViewModel

--- Africanacity_Backend/Africanacity_Team24(INF370)/View Models/EmployeeViewModel.cs
+++ Africanacity_Backend/Africanacity_Team24(INF370)/View Models/EmployeeViewModel.cs
@@ -1,7 +1,14 @@
-﻿namespace Africanacity_Team24_INF370_.ViewModel
+﻿using Org.BouncyCastle.Bcpg.OpenPgp;
+
+namespace Africanacity_Team24_INF370_.ViewModel
 {
     public class EmployeeViewModel
     {
-
+        public int EmployeeId { get; set; }
+        public string? Name { get; set; }
+        public string? Surname { get; set; }
+        public string? Email_Address { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Physical_Address { get; set; }
     }
 }
