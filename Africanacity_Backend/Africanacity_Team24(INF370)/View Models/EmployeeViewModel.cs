70a68c744857311ccc250cb5922d6acd8409218c
u21481742 <u21481742@tuks.co.za>
2023-05-16 13:38:02 +0200

    Changes Made in the LaunchSettings.Json. Done to enable Swagger.

--- Africanacity_Backend/Africanacity_Team24(INF370)/View Models/EmployeeViewModel.cs
+++ Africanacity_Backend/Africanacity_Team24(INF370)/View Models/EmployeeViewModel.cs
@@ -5,10 +5,10 @@ namespace Africanacity_Team24_INF370_.ViewModel
     public class EmployeeViewModel
     {
         public int EmployeeId { get; set; }
-        public string Name { get; set; }
-        public string Surname { get; set; }
-        public string Email_Address { get; set; }
-        public string PhoneNumber { get; set; }
-        public string Physical_Address { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Surname { get; set; } = string.Empty;
+        public string? Email_Address { get; set; }
+        public string PhoneNumber { get; set; } = string.Empty;
+        public string? Physical_Address { get; set; }
     }
 }
