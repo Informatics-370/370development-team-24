2ea4cc912e81cc9cd3232d2925973316e5a46b8b
u21663841 <u21663841@tuks.co.za>
2023-05-17 02:01:13 +0200

    completed CRUDS

--- Africanacity_Backend/Africanacity_Team24(INF370)/Controllers/EmployeeRoleController.cs
+++ Africanacity_Backend/Africanacity_Team24(INF370)/Controllers/EmployeeRoleController.cs
@@ -2,13 +2,14 @@
 using Microsoft.AspNetCore.Mvc;
 using Africanacity_Team24_INF370_.models.Administration;
 using Africanacity_Team24_INF370_.ViewModel;
+using Africanacity_Team24_INF370_.View_Models;
 
 namespace Africanacity_Team24_INF370_.Controllers
 {
         [Route("api/[controller]")]
         [ApiController]
-        public class EmployeeRoleController : ControllerBase
-        {
+      public class EmployeeRoleController : ControllerBase
+      {
             private readonly IRepository _Repository;
 
             public EmployeeRoleController(IRepository Repository)
@@ -53,7 +54,7 @@ namespace Africanacity_Team24_INF370_.Controllers
            [Route("AddEmployeeRole")]
            public async Task<IActionResult> AddEmployeeRole(EmployeeRoleViewModel cvm)
            {
-             var Employee_Role = new Employee_Role { Employee_RoleId = cvm, Name = cvm.Name, Description = cvm.Description };
+             var Employee_Role = new Employee_Role { Employee_RoleId = cvm.Employee_RoleId, Name = cvm.Name, Description = cvm.Description };
 
              try
              {
@@ -67,14 +68,58 @@ namespace Africanacity_Team24_INF370_.Controllers
 
               return Ok(Employee_Role);
            }
-           
-           
+
+           [HttpPut]
+           [Route("EditEmployeeRole/{Employee_RoleId}")]
+           public async Task<ActionResult<EmployeeRoleViewModel>> EditEmployeeRole(int Employee_RoleId, EmployeeRoleViewModel employeeRoleModel)
+           {
+              try
+              {
+                var existingEmployeeRole = await _Repository.GetEmployeeRoleAsync(Employee_RoleId);
+                if (existingEmployeeRole == null) return NotFound($"The employee role does not exist");
+
+                existingEmployeeRole.Employee_RoleId = employeeRoleModel.Employee_RoleId;
+                existingEmployeeRole.Name = employeeRoleModel.Name;
+                existingEmployeeRole.Description = employeeRoleModel.Description;
+
+                if (await _Repository.SaveChangesAsync())
+                {
+                    return Ok(existingEmployeeRole);
+                }
+              }
+             catch (Exception)
+              {
+                return StatusCode(500, "Internal Server Error. Please contact support.");
+              }
+             return BadRequest("Your request is invalid.");
+           }
+
+            [HttpDelete]
+            [Route("DeleteEmployeeRole/{Employee_RoleId}")]
+            public async Task<IActionResult> DeleteEmployeeRole(int Employee_RoleId)
+            {
+                try
+                {
+                  var existingEmployeeRole = await _Repository.GetEmployeeRoleAsync(Employee_RoleId);
+
+                  if (existingEmployeeRole == null) return NotFound($"The employee role does not exist");
+
+                   _Repository.Delete(existingEmployeeRole);
+
+                   if (await _Repository.SaveChangesAsync()) return Ok(existingEmployeeRole);
+
+                }
+                catch (Exception)
+                {
+                   return StatusCode(500, "Internal Server Error. Please contact support.");
+                }
+                return BadRequest("Your request is invalid.");
+            }
 
 
 
 
 
-
-        }
+      }
 
 }
--- Africanacity_Backend/Africanacity_Team24(INF370)/View Models/EmployeeRoleViewModel.cs
+++ Africanacity_Backend/Africanacity_Team24(INF370)/View Models/EmployeeRoleViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
+
 namespace Africanacity_Team24_INF370_.View_Models
 {
     public class EmployeeRoleViewModel
@@ -8,6 +9,8 @@ namespace Africanacity_Team24_INF370_.View_Models
         public int Employee_RoleId { get; set; }
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
+
+        public int EmployeeId { get; set; }
 
     }
 }
