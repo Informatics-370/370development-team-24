6168eb9df47f1608ddd8c6cb0956e25cce80061b
u21612472 <102991673+u21612472@users.noreply.github.com>
2023-05-19 19:31:22 +0200

    MenuType View changes

--- Africanacity_Backend/Africanacity_Team24(INF370)/View Models/MenuItemViewModel.cs
+++ Africanacity_Backend/Africanacity_Team24(INF370)/View Models/MenuItemViewModel.cs
@@ -6,11 +6,7 @@ namespace Africanacity_Team24_INF370_.View_Models
     {
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
-        public List<Menu_Type> MenuTypes { get; set; } = new List<Menu_Type>();
-
-        public List<Food_Type> FoodTypes { get; set; } = new List<Food_Type>();
-
-        public List<MenuItem_Category> MenuItem_Categories { get; set; } = new List<MenuItem_Category>();
+      
 
     }
 }
---
+++ Africanacity_Backend/Africanacity_Team24(INF370)/View Models/MenuTypeViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Africanacity_Team24_INF370_.View_Models
+{
+    public class MenuTypeViewModel
+    {
+        
+        
+    }
+}
