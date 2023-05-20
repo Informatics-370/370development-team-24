aa0418d0194d15c3343a70eec52211c5e596dba8
u21663841 <u21663841@tuks.co.za>
2023-05-20 03:46:35 +0200

    new changes made angular

--- Africanacity_Frontend/src/app/app-routing.module.ts
+++ Africanacity_Frontend/src/app/app-routing.module.ts
@@ -10,6 +10,9 @@ import { RegisterComponent } from './login/register/register.component';
 import { HomeComponent } from './administration/home/home.component'; // for home page
 import { MenuComponent } from './administration/menu/menu.component'; // for menu page
 
+//routing for employee role pages
+
+
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent }, // for home page
