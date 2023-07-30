bb8122f0cd4827e56fbcf3fe89c3c7eeddbcb1a1
u21481742 <u21481742@tuks.co.za>
2023-07-30 11:58:40 +0200

    Changes Made in the backend
    Migration deleted

--- Africanacity_Backend/Africanacity_Team24(INF370)/View Models/SupplierInventoryVM.cs
+++ Africanacity_Backend/Africanacity_Team24(INF370)/View Models/SupplierInventoryVM.cs
@@ -6,8 +6,8 @@
         public int ItemName { get; set; }
         public int SupplierNames { get; set; }
         public string Ordered_Date { get; set; }
-        public string Received_Date { get; set; }
+        public string Received_Date { get; set; } // Change this to string
         public int Ordered_Quantity { get; set; }
     }
-  
+
 }
