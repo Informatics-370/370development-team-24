ee796bb7f1cb638f074169a531468fbd69b639ce
21492362 <102881817+u21492362@users.noreply.github.com>
2023-09-03 12:33:21 +0200

    Booking

--- Africanacity_Backend/Africanacity_Team24(INF370)/View Models/DrinkViewModel.cs
+++ Africanacity_Backend/Africanacity_Team24(INF370)/View Models/DrinkViewModel.cs
@@ -3,8 +3,11 @@ using Africanacity_Team24_INF370_.models.Restraurant;
 
 namespace Africanacity_Team24_INF370_.View_Models
 {
-    public class DrinkViewModel
+    public class OtherDrinkViewModel
     {
+        public int OtherDrinkId { get; set; }
+
+        public 
         public string Name { get; set; } = string.Empty;
         public int DrinkType { get; set; } 
         public int DrinkId { get; set; }
