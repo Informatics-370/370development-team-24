6c92cc1dbb94cdbd84db63724e458873562aac1e
21498629 <102926514+21498629@users.noreply.github.com>
2023-10-17 21:07:51 +0200

    Revert "Refilwe"

---
+++ Africanacity_Backend/Africanacity_Team24(INF370)/models/Restraurant/Order_MenuItem.cs
@@ -0,0 +1,22 @@
+﻿using Africanacity_Team24_INF370_.models.Administration.Admin;
+using System.ComponentModel.DataAnnotations;
+
+namespace Africanacity_Team24_INF370_.models.Restraurant
+{
+    public class Order_MenuItem
+	{
+
+		[Key]
+		public int OrderMenuItemId { get; set; }
+		public int Quantity { get; set; }
+
+		public int MenuItemId { get; set; }
+		public MenuItem MenuItem { get; set; }
+
+		public int KitchenOrderId { get; set; }
+
+		public KitchenOrder KitchenOrder { get; set; }
+
+		
+	}
+}
